41b6fc81f06111f2374ac8007c9ff7b7f5a82e0c
Kbz-8 <kbz_8.dev@akel-engine.com>
2025-05-13 17:12:06 +0200

    updating

--- src/app/components/molecules/pagination.rs
+++ src/app/components/molecules/pagination.rs
@@ -1,5 +1,6 @@
 use dioxus::prelude::*;
 use dioxus_tw_components::molecules::pagination::*;
+use dioxus_tw_components::atoms::{Button, ButtonVariant, button::ButtonProps};
 
 use crate::app::{components::preview::*, doctrait::DemoComponent};
 
@@ -24,13 +25,39 @@ impl DemoComponent for PaginationProps {
 
     fn BuildCompPreview() -> Element {
         let page_number = use_signal(|| 1_usize);
+        let page_number_outlined = use_signal(|| 1_usize);
+        let page_number_ghost = use_signal(|| 1_usize);
+        let state = use_context::<Signal<HashPreview>>();
 
         rsx! {
-            div { class: "border rounded-global-radius w-full h-fit",
+            div { class: "flex flex-col space-y-8 w-full h-fit",
                 Pagination {
+                    class: state.read()[&0].get_class(),
+                    color: state.read()[&0].get_color(),
+                    size: state.read()[&0].get_size(),
                     data_size: 1000_usize,
                     page_size: 25_usize,
                     page_number,
+                }
+
+                Pagination {
+                    class: state.read()[&0].get_class(),
+                    color: state.read()[&0].get_color(),
+                    size: state.read()[&0].get_size(),
+                    variant: ButtonVariant::Outline,
+                    data_size: 1000_usize,
+                    page_size: 25_usize,
+                    page_number: page_number_outlined,
+                }
+
+                Pagination {
+                    class: state.read()[&0].get_class(),
+                    color: state.read()[&0].get_color(),
+                    size: state.read()[&0].get_size(),
+                    variant: ButtonVariant::Ghost,
+                    data_size: 1000_usize,
+                    page_size: 25_usize,
+                    page_number: page_number_ghost,
                 }
             }
         }
